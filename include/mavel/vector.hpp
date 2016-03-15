96be13ded7fb14a1606c1b345e8ec1e06b41b012
Petr Kmoch <petr.kmoch@gmail.com>
2016-03-15 22:16:44 +0100

    Remove traces of SIMD
    Remove stubs of SIMD-related code; that was confined to a separate branch.

--- include/mavel/vector.hpp
+++ include/mavel/vector.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "mavel/operation_set.hpp"
+#include "mavel/operation_set_selector.hpp"
+
 #include <cstddef>
 
 
@@ -9,7 +12,6 @@ template <
 	class Component,
 	::std::size_t Dimension,
 	operation_set OperationSet = operation_set_selector<Component>::value,
-	class SimdUsage = iffast_simd_t<OperationSet>
 >
 class vector;
 
